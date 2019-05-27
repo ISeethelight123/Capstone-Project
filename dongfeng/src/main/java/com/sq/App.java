3b1583d0e231f0fe614a4ad5c7b7d23125b86ce8
pkd <x009988@163.com>
2019-05-27 14:26:42 +0800

    c

--- dongfeng/src/main/java/com/sq/App.java
+++ dongfeng/src/main/java/com/sq/App.java
@@ -1,8 +1,10 @@
 package com.sq;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sq.mapper")
 public class App {
 
 	public static void main(String[] args) throws Exception {
